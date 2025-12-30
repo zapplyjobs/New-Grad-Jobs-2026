26d8a40645ab281323060bf7770303dc6c037e61
z-apply <admin@zapply.jobs>
2025-12-30 13:41:55 +0500

    top companies fix

--- .github/scripts/job-fetcher/readme-generator.js
+++ .github/scripts/job-fetcher/readme-generator.js
@@ -581,19 +581,19 @@ ${generateJobTable(currentJobs)}
 ### 🏢 Top Companies
 
 #### 🌟 FAANG+
-${companies.faang_plus.map((c) => `${c.emoji} [${c.name}](${c.career_url}) (${currentJobs.filter(job => job.employer_name === c.name).length})`).join(" • ")}
+${companies.faang_plus.map((c) => `${c.emoji} [${c.name}](${c.career_url}) (${currentJobs.filter(job => job.employer_name.toLowerCase().includes(c.name.toLowerCase())).length})`).join(" • ")}
 
 #### 🦄 Unicorn Startups
-${companies.unicorn_startups.map((c) => `${c.emoji} [${c.name}](${c.career_url}) (${currentJobs.filter(job => job.employer_name === c.name).length})`).join(" • ")}
+${companies.unicorn_startups.map((c) => `${c.emoji} [${c.name}](${c.career_url}) (${currentJobs.filter(job => job.employer_name.toLowerCase().includes(c.name.toLowerCase())).length})`).join(" • ")}
 
 #### 💰 Fintech Leaders
-${companies.fintech.map((c) => `${c.emoji} [${c.name}](${c.career_url}) (${currentJobs.filter(job => job.employer_name === c.name).length})`).join(" • ")}
+${companies.fintech.map((c) => `${c.emoji} [${c.name}](${c.career_url}) (${currentJobs.filter(job => job.employer_name.toLowerCase().includes(c.name.toLowerCase())).length})`).join(" • ")}
 
 #### 🎮 Gaming & Entertainment
-${[...companies.gaming, ...companies.media_entertainment].map((c) => `${c.emoji} [${c.name}](${c.career_url}) (${currentJobs.filter(job => job.employer_name === c.name).length})`).join(" • ")}
+${[...companies.gaming, ...companies.media_entertainment].map((c) => `${c.emoji} [${c.name}](${c.career_url}) (${currentJobs.filter(job => job.employer_name.toLowerCase().includes(c.name.toLowerCase())).length})`).join(" • ")}
 
 #### ☁️ Enterprise & Cloud
-${[...companies.top_tech, ...companies.enterprise_saas].map((c) => `${c.emoji} [${c.name}](${c.career_url}) (${currentJobs.filter(job => job.employer_name === c.name).length})`).join(" • ")}
+${[...companies.top_tech, ...companies.enterprise_saas].map((c) => `${c.emoji} [${c.name}](${c.career_url}) (${currentJobs.filter(job => job.employer_name.toLowerCase().includes(c.name.toLowerCase())).length})`).join(" • ")}
 
 ---
 
