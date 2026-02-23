43c2daede6502c6d8bc1d2ad79bd3f11cab8d6de
z-apply <admin@zapply.jobs>
2026-02-23 08:19:15 -0600

    Update button sizes and website URL to ?ref=github

--- .github/scripts/job-fetcher/readme-generator.js
+++ .github/scripts/job-fetcher/readme-generator.js
@@ -402,7 +402,7 @@ ${config.tagline}
 
 </div>
 
-<p align="center">${renderedConfig.descriptionLine1}${config.descriptionLine1.includes('Welcome') ? ' by <a href="https://zapply.jobs"><img src="https://zapply.jobs/_astro/logo-white.BELjrjiH_Z18qziS.svg" alt="Zapply logo" height="20" align="center"></a>' : ''}</p>
+<p align="center">${renderedConfig.descriptionLine1}${config.descriptionLine1.includes('Welcome') ? ' by <a href="https://zapply.jobs/?ref=github"><img src="https://zapply.jobs/_astro/logo-white.BELjrjiH_Z18qziS.svg" alt="Zapply logo" height="20" align="center"></a>' : ''}</p>
 
 ${renderedConfig.descriptionLine2 ? `<p align="center">${renderedConfig.descriptionLine2}</p>\n\n` : ''}> [!${config.noteType}]
 > ${config.noteText}
@@ -423,9 +423,9 @@ Explore Zapply's website and check out:
 Experience an advanced career journey with us! 🚀
 
 <p align="center">
-  <a href="https://zapply.jobs/"><img src="images/zapply-button.png" alt="Visit Our Website" height="60"></a>
+  <a href="https://zapply.jobs/?ref=github"><img src="images/zapply-button.png" alt="Visit Our Website" width="300"></a>
   &nbsp;&nbsp;&nbsp;&nbsp;
-  <a href="https://chromewebstore.google.com/detail/zapply-instant-autofill-f/lkomdndabnpakcabffgobiejimpamjom"><img src="images/extension-button.png" alt="Install Our Extension" height="60"></a>
+  <a href="https://chromewebstore.google.com/detail/zapply-instant-autofill-f/lkomdndabnpakcabffgobiejimpamjom"><img src="images/extension-button.png" alt="Install Our Extension" width="277"></a>
 </p>
 
 ---
@@ -437,11 +437,11 @@ Experience an advanced career journey with us! 🚀
 Connect and seek advice from a growing network of fellow students and new grads.
 
 <p align="center">
-  <a href="https://discord.gg/UswBsduwcD"><img src="images/discord-2d.png" alt="Visit Our Website" height="60"></a>
+  <a href="https://discord.gg/UswBsduwcD"><img src="images/discord-2d.png" alt="Visit Our Website" width="250"></a>
   &nbsp;&nbsp;
-  <a href="https://www.instagram.com/zapplyjobs"><img src="images/instagram-icon-2d.png" alt="Instagram" height="60"></a>
+  <a href="https://www.instagram.com/zapplyjobs"><img src="images/instagram-icon-2d.png" alt="Instagram" width="75"></a>
   &nbsp;&nbsp;
-  <a href="https://www.tiktok.com/@zapplyjobs"><img src="images/tiktok-icon-2d.png" alt="TikTok" height="60"></a>
+  <a href="https://www.tiktok.com/@zapplyjobs"><img src="images/tiktok-icon-2d.png" alt="TikTok" width="75"></a>
 </p>
 
 ---
